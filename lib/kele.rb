a9b45f35f52d6b09b5fe51d6dd359b4d80b18584
Aske Bentzen <askebentzen@gmail.com>
2017-11-27 20:23:18 +0100

    fixed

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,11 +2,14 @@ require 'HTTParty'
 
   class Kele
     include HTTParty
+    base_uri 'https://www.bloc.io/api/v1'
+    debug_output
 
     def initialize(email, password)
-      response = self.class.post("https://www.bloc.io/api/v1/sessions", body: { email: email, password: password })
+      response = self.class.post('/sessions', body: { email: email, password: password })
       @auth_token = response['auth_token']
-      puts ArgumentError
+      if response.nil? || response['auth_token'].nil?
+         raise ArgumentError.new("The system was unable to authorize you.")
+      end
     end
-
   end
